aea7b66c9289cebc08ed6bd3d8a65ed8ecc23776
DeRyckYannick <yannick.deryck@student.kdg.be>
2025-02-18 16:39:02 +0100

    mvp fixed + auto movement beetje

---
+++ src/main/java/be/kdg/project/rushhour/mvp/view/gameScreen/GamePresenter.java
@@ -0,0 +1,15 @@
+package be.kdg.project.rushhour.mvp.view.gameScreen;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GamePresenter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
