a7eec5cfc310f39c5f97f1236d54bf3dcd17e43c
DeRyckYannick <yannick.deryck@student.kdg.be>
2025-02-22 19:14:06 +0100

    auto's kunnen niet botsen

--- src/main/java/be/kdg/project/rushhour/mvp/view/gameScreen/AutoHandler.java
+++ src/main/java/be/kdg/project/rushhour/mvp/view/gameScreen/AutoHandler.java
@@ -3,22 +3,19 @@ package be.kdg.project.rushhour.mvp.view.gameScreen;
 import be.kdg.project.rushhour.mvp.model.Auto;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.Node;
 import javafx.scene.shape.Rectangle;
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class AutoHandler {
     private GridPane speelveld;
     private Auto auto;
-    private List<Rectangle> rects;
     private GamePresenter presenter;
 
     public AutoHandler(GridPane speelveld, Auto auto, GamePresenter presenter) {
         this.speelveld = speelveld;
         this.auto = auto;
         this.presenter = presenter;
-        this.rects = new ArrayList<>();
         speelveld.setOnMouseClicked(this::handleMouseClick);
     }
 
@@ -26,57 +23,36 @@ public class AutoHandler {
         int clickedX = (int) event.getX() / 50;
         int clickedY = (int) event.getY() / 50;
 
-        if (isClickedOnFront(clickedX, clickedY)) {
-            moveAutoForward();
-        } else if (isClickedOnBack(clickedX, clickedY)) {
-            moveAutoBackward();
-        }
-    }
-
-    private void tekenAuto() {
-        verwijderAutoVanGrid();
-        rects.clear();
-
-        for (int i = 0; i < auto.getLengte(); i++) {
-            Rectangle rect = new Rectangle(50, 50, auto.getKleur());
-            rects.add(rect);
-
-            if (auto.isHorizontaal()) {
-                speelveld.add(rect, auto.getxPos() + i, auto.getyPos());
-            } else {
-                speelveld.add(rect, auto.getxPos(), auto.getyPos() + i);
-            }
+        if (isClickedOnFront(clickedX, clickedY) && canMoveForward()) {
+            auto.moveForward();
+            presenter.updateView();
+        } else if (isClickedOnBack(clickedX, clickedY) && canMoveBackward()) {
+            auto.moveBackward();
+            presenter.updateView();
         }
     }
 
     private boolean isClickedOnFront(int x, int y) {
-        return auto.isHorizontaal()
-                ? x == auto.getxPos() + auto.getLengte() - 1 && y == auto.getyPos()
-                : y == auto.getyPos() + auto.getLengte() - 1 && x == auto.getxPos();
+        if (auto.isHorizontaal()) {
+            return x == auto.getxPos() + auto.getLengte() - 1 && y == auto.getyPos();
+        } else {
+            return y == auto.getyPos() + auto.getLengte() - 1 && x == auto.getxPos();
+        }
     }
 
     private boolean isClickedOnBack(int x, int y) {
-        return auto.isHorizontaal()
-                ? x == auto.getxPos() && y == auto.getyPos()
-                : y == auto.getyPos() && x == auto.getxPos();
-    }
-
-    private void moveAutoForward() {
-        if (auto.canMoveForward(6)) {
-            auto.moveForward();
-            presenter.updateView(); // Update het hele speelveld via de GamePresenter
+        if (auto.isHorizontaal()) {
+            return x == auto.getxPos() && y == auto.getyPos();
+        } else {
+            return y == auto.getyPos() && x == auto.getxPos();
         }
     }
 
-    private void moveAutoBackward() {
-        if (auto.canMoveBackward()) {
-            auto.moveBackward();
-            presenter.updateView(); // Update het hele speelveld via de GamePresenter
-        }
+    private boolean canMoveForward() {
+        return presenter.isMovePossible(auto, true);
     }
 
-    private void verwijderAutoVanGrid() {
-        List<Node> teVerwijderen = new ArrayList<>(rects);
-        speelveld.getChildren().removeAll(teVerwijderen);
+    private boolean canMoveBackward() {
+        return presenter.isMovePossible(auto, false);
     }
 }
--- src/main/java/be/kdg/project/rushhour/mvp/view/gameScreen/GamePresenter.java
+++ src/main/java/be/kdg/project/rushhour/mvp/view/gameScreen/GamePresenter.java
@@ -18,20 +18,57 @@ public class GamePresenter {
     }
 
     private void initializeAutos() {
-        Auto rodeAuto = new Auto(0, 2, 2, true, Color.RED); // Rode auto
-        Auto blauweAuto = new Auto(2, 0, 2, false, Color.BLUE); // Blauwe auto
+        Auto rodeAuto = new Auto(0, 2, 2, true, Color.RED);
+        Auto blauweAuto = new Auto(2, 0, 2, false, Color.BLUE);
 
         autos.add(rodeAuto);
         autos.add(blauweAuto);
 
         for (Auto auto : autos) {
-            new AutoHandler(view.getSpeelveld(), auto, this); // Koppel een AutoHandler aan elke auto
+            new AutoHandler(view.getSpeelveld(), auto, this);
         }
 
         updateView();
     }
 
     public void updateView() {
-        view.updateSpeelveld(autos); // Update het hele speelveld
+        view.updateSpeelveld(autos);
+    }
+
+    public boolean isMovePossible(Auto auto, boolean forward) {
+        int newX = auto.getxPos();
+        int newY = auto.getyPos();
+
+        if (forward) {
+            if (auto.isHorizontaal()) {
+                newX += auto.getLengte();
+            } else {
+                newY += auto.getLengte();
+            }
+        } else {
+            if (auto.isHorizontaal()) {
+                newX -= 1;
+            } else {
+                newY -= 1;
+            }
+        }
+
+        if (newX < 0 || newX >= 6 || newY < 0 || newY >= 6) {
+            return false;
+        }
+
+        for (Auto other : autos) {
+            if (other != auto) {
+                for (int i = 0; i < other.getLengte(); i++) {
+                    int occupiedX = other.isHorizontaal() ? other.getxPos() + i : other.getxPos();
+                    int occupiedY = other.isHorizontaal() ? other.getyPos() : other.getyPos() + i;
+
+                    if (occupiedX == newX && occupiedY == newY) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
