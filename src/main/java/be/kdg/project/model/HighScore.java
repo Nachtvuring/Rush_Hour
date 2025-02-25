858ff3adb9fc9750d11088b569df25bb89af74fa
Nachtvuring <landersegers1@telenet.be>
2025-02-25 16:28:34 +0100

    Player name and score updated highscore needs to be fixed

---
+++ src/main/java/be/kdg/project/model/HighScore.java
@@ -0,0 +1,15 @@
+package be.kdg.project.model;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class HighScore extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
